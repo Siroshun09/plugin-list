420e312cd7ad5803a74eedc7f543305466cf41aa
Siroshun09 <siro.shun8@gmail.com>
2024-04-22 00:09:37 +0900

    feat(backend): implements MCPlugin repository using SQLite database

---
+++ backend/repository/sqlite/sqlite.go
@@ -0,0 +1,141 @@
+package sqlite
+
+import (
+	"context"
+	"database/sql"
+	"github.com/Siroshun09/plugin-list/domain"
+	"github.com/Siroshun09/plugin-list/repository"
+	_ "modernc.org/sqlite"
+	"time"
+)
+
+const (
+	ImplementationName = "sqlite"
+	DatabaseFilename   = "sqlite.db"
+)
+
+type Connection interface {
+	IsOpen() bool
+	Close() error
+	NewMCPluginRepository() (repository.MCPluginRepository, error)
+}
+
+type sqliteConnection struct {
+	db *sql.DB
+}
+
+func CreateConnection(filepath string) (Connection, error) {
+	db, err := sql.Open("sqlite", filepath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &sqliteConnection{db}, nil
+}
+
+func (c *sqliteConnection) IsOpen() bool {
+	return c.db.Ping() == nil
+}
+
+func (c *sqliteConnection) Close() error {
+	return c.db.Close()
+}
+
+const (
+	mcPluginTableSchema = `
+		CREATE TABLE IF NOT EXISTS mc_plugins (
+			plugin_name VARCHAR(32) NOT NULL,
+			server_name VARCHAR(16) NOT NULL,
+			filename VARCHAR(64) NOT NULL,
+			version VARCHAR(32) NOT NULL,
+			type VARCHAR(16) NOT NULL,
+			last_updated INTEGER NOT NULL,
+			PRIMARY KEY (plugin_name, server_name)
+		)
+		`
+	createOrUpdateMcPluginQuery = `
+		INSERT INTO mc_plugins (plugin_name, server_name, filename, version, type, last_updated) VALUES (?, ?, ?, ?, ?, ?) 
+		ON CONFLICT (plugin_name, server_name) DO UPDATE SET filename = excluded.filename, version = excluded.version,
+		type = excluded.type, last_updated = excluded.last_updated
+		`
+
+	deleteMcPluginQuery = `DELETE FROM mc_plugins WHERE plugin_name = ? AND server_name = ?`
+
+	selectMCPluginsByServerName = "SELECT * FROM mc_plugins WHERE server_name=?"
+)
+
+func (c *sqliteConnection) NewMCPluginRepository() (repository.MCPluginRepository, error) {
+	if _, err := c.db.Exec(mcPluginTableSchema); err != nil {
+		return nil, err
+	}
+
+	return mcPluginRepository{c}, nil
+}
+
+type mcPluginRepository struct {
+	conn *sqliteConnection
+}
+
+func (m mcPluginRepository) CreateOrUpdateMCPlugin(_ context.Context, plugin *domain.MCPlugin) (returnErr error) {
+	rows, err := m.conn.db.Query(createOrUpdateMcPluginQuery, plugin.PluginName, plugin.ServerName, plugin.FileName, plugin.Version, plugin.Type, plugin.LastUpdated.UnixMilli())
+
+	if err != nil {
+		return err
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			returnErr = err
+		}
+	}(rows)
+
+	return nil
+}
+
+func (m mcPluginRepository) DeleteMCPlugin(ctx context.Context, serverName string, pluginName string) (returnErr error) {
+	rows, err := m.conn.db.Query(deleteMcPluginQuery, pluginName, serverName)
+
+	if err != nil {
+		return err
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			returnErr = err
+		}
+	}(rows)
+
+	return nil
+}
+
+func (m mcPluginRepository) GetMCPluginsByServerName(_ context.Context, serverName string) (plugins []*domain.MCPlugin, returnErr error) {
+	rows, err := m.conn.db.Query(selectMCPluginsByServerName, serverName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			returnErr = err
+		}
+	}(rows)
+
+	var result []*domain.MCPlugin
+
+	for rows.Next() {
+		var plugin domain.MCPlugin
+		var unixTime int64
+		if err := rows.Scan(&plugin.PluginName, &plugin.ServerName, &plugin.FileName, &plugin.Version, &plugin.Type, &unixTime); err != nil {
+			return nil, err
+		}
+		plugin.LastUpdated = time.UnixMilli(unixTime)
+		result = append(result, &plugin)
+	}
+
+	return result, nil
+}
--- backend/usecase/mc_plugin_usecase.go
+++ backend/usecase/mc_plugin_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 	"github.com/Siroshun09/plugin-list/domain"
+	"github.com/Siroshun09/plugin-list/repository"
 )
 
 // MCPluginUseCase はプラグイン情報の取得や更新を行います。
@@ -13,4 +14,24 @@ type MCPluginUseCase interface {
 	DeleteMCPlugin(ctx context.Context, serverName string, pluginName string) error
 	// SubmitMCPlugin はプラグイン情報を作成 (Create) または更新 (Update) します。
 	SubmitMCPlugin(ctx context.Context, plugin *domain.MCPlugin) error
+}
+
+func NewMCPluginUseCase(repo repository.MCPluginRepository) MCPluginUseCase {
+	return repositoryUseCase{repo}
+}
+
+type repositoryUseCase struct {
+	repo repository.MCPluginRepository
+}
+
+func (r repositoryUseCase) GetMCPluginsByServerName(ctx context.Context, serverName string) ([]*domain.MCPlugin, error) {
+	return r.repo.GetMCPluginsByServerName(ctx, serverName)
+}
+
+func (r repositoryUseCase) DeleteMCPlugin(ctx context.Context, serverName string, pluginName string) error {
+	return r.repo.DeleteMCPlugin(ctx, serverName, pluginName)
+}
+
+func (r repositoryUseCase) SubmitMCPlugin(ctx context.Context, plugin *domain.MCPlugin) error {
+	return r.repo.CreateOrUpdateMCPlugin(ctx, plugin)
 }
